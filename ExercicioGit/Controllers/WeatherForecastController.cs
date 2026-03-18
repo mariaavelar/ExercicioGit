7fd5297cdab122739c7f234c62e2d255a94e34a9
maria.gomes <avelarmarialuiza12345@gmail.com>
2026-03-18 15:35:13 -0300

    troquei o friz por frio

--- ExercicioGit/Controllers/WeatherForecastController.cs
+++ ExercicioGit/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace ExercicioGit.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Frio", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
